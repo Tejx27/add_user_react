fe7f7ee6b1d71bb7d208e34fe90ccdb7cae22069
Tejas Nikam <Tejasnikam4515@gmail.com>
2023-02-10 17:45:03 +0530

    add error feature.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import AddUser from "./Components/Users/AddUser";
+import UserList from './Components/Users/UsersList';
 
 function App() {
+  const [usersList,setUsersList] =  useState([]);
+
+  const addUserHandler = (usrName,usrAge) =>{
+      setUsersList((prevUserList) => {
+        return [...prevUserList,{name:usrName,age:usrAge,id: Math.random.toString()}];
+      });
+  }
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler}/>
+      <UserList users={usersList}/>
     </div>
   );
 }
